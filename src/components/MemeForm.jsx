221374f1af2ab10997bcf0a09e4d76c902ed3d63
Fleege <76089935+pfleege@users.noreply.github.com>
2024-09-20 15:22:52 +0300

    ADD useState to MemeForm.jsx to capture form data

--- src/components/MemeForm.jsx
+++ src/components/MemeForm.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 const MemeForm = () => {
+  const [formData, setFormData] = useState({
+    memeHeading: "",
+    memeBody: "",
+  });
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+  }
+
   return (
-    <form className="memeForm" action="">
+    <form className="memeForm" onSubmit={handleSubmit}>
       <div className="memeForm--inputFields">
         <label>
           Your Meme Heading:
           <input
             className="memeForm--textField"
             type="text"
+            name="memeHeading"
             placeholder="Meme heading"
+            value={formData.memeHeading}
+            onChange={handleChange}
           />
         </label>
         <label>
@@ -15,7 +40,10 @@ const MemeForm = () => {
           <input
             className="memeForm--textField"
             type="text"
+            name="memeBody"
             placeholder="Meme body text"
+            value={formData.memeBody}
+            onChange={handleChange}
           />
         </label>
       </div>
